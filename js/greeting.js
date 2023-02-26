c2bd2e5e6fba330bf54ed50748ec1ebde41f3b60
silverstarlulu <117253988+silverstarlulu@users.noreply.github.com>
2023-02-27 00:51:29 +0900

    greeting

--- js/greeting.js
+++ js/greeting.js
@@ -7,22 +7,22 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault(); // 브라우저의 기본 동작을 막아줌(ex 새로고침)
-  const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem("USERNAME_KEY", username);
-  paintGreetings(username);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreetings();
 }
 
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello, ${username}!!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem("USERNAME_KEY");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  paintGreetings(savedUsername);
+  paintGreetings();
 }
