d9c4e8c9261ba7a6a318bcbd20c5fec8e698ec84
silverstarlulu <117253988+silverstarlulu@users.noreply.github.com>
2023-02-27 01:20:11 +0900

    clock

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); //즉시 호출
+setInterval(getClock, 1000); //이후에 1초마다 실행
